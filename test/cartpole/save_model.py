23cff06de0b763fe059fd350bca737a84130c4cc
TroddenSpade <troddenspade@gmail.com>
2023-02-14 22:59:09 +0300

    updated

--- test/cartpole/save_model.py
+++ test/cartpole/save_model.py
@@ -1,22 +1,24 @@
 from email import policy
-from functions.write_params import write_params
+from write_params import write_params
 
 import torch
 import gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+import numpy as np
 
-env = gym.make('CartPole-v1')
+# env = gym.make('CartPole-v1')
 
 # policy_kwargs = dict(net_arch=[dict(pi=[12, 12], vf=[12, 12])])
 # model = PPO("MlpPolicy", env, verbose=1, policy_kwargs=policy_kwargs)
 # model.learn(total_timesteps=250000)
 # model.save("ppo_cartpole")
 
-# params = ["mlp_extractor.policy_net.0.weight", "mlp_extractor.policy_net.0.bias", "mlp_extractor.policy_net.2.weight", "mlp_extractor.policy_net.2.bias", "action_net.weight", "action_net.bias"]
-# write_params("w.h", model.policy, params)
 
-model = PPO.load("ppo_cartpole", env=env)
+model = PPO.load("ppo_cheetah")
+
+params = ["mlp_extractor.policy_net.0.weight", "mlp_extractor.policy_net.0.bias", "mlp_extractor.policy_net.2.weight", "mlp_extractor.policy_net.2.bias", "action_net.weight", "action_net.bias"]
+# write_params("w.h", model.policy, params)
 
 def forward(model, obs):
     with torch.no_grad():
@@ -32,19 +34,14 @@ def obs_to_str(obs):
 
 
 rews = 0
-obs, info = env.reset()
 i = 0
-while True:
+for i in range(20):
+    obs = np.random.randn(17)
     print(obs_to_str(obs))
     obs = torch.from_numpy(obs).unsqueeze(0).float()
     pi = forward(model.policy, obs)
+    print(pi)
     a1 = pi.argmax(dim=1).item()
     
-    obs, reward, terminated, truncated, info = env.step(a1)
-    rews += reward
-
-    if terminated or truncated or (i > 100):
-        break
 
 print(rews)
-env.close()
